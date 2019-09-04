9197c5f4c77ede8309df3d94f3365fef7be1eb95
jeewooj <54431132+jeewooj@users.noreply.github.com>
2019-09-04 18:25:57 +0900

    portfolio mims/gw 추가

--- pf_open.php
+++ pf_open.php
@@ -25,11 +25,49 @@
                         <li>중동시장을 겨냥한 부동산을 검색, 매매할 수 있는 플랫폼</li>
                         <li>영어버전의 PC/mobile, 아랍어버전어 PC/mobile 총 4가지 버전의 전체 대략 500페이지 정도의 홈페이지</li>
                         <li>아랍어버전은 번역 뿐만 아니라 홈페이지 구성을 전부 좌우반전하는 작업을 하였다.</li>
+                        <li>제플린 사용하여 작업</li>
                         <li>시작부터 검수까지 참여하였으며 기여도 80%</li>
                         <li>작업기간 3개월</li>
                     </ul>
                 </div>
             </article>
+            <article class="pfArea pjec mims right" onclick="location.href='./pf/mims.php'">
+                <div class="pf_imgBx">
+                    <div class="pf_img tb"><div class="imgBx"><img src="./imgs/pf/mims/mims_use_1.png" alt="Mims user page"></div></div>
+                    <div class="pf_img pc"><div class="imgBx"><img src="./imgs/pf/mims/mims_solution_1.png" alt="Mims solution page"></div></div>
+                </div>
+                <div class="pf_tit">
+                    <h3>MIMS</h3>
+                    <ul class="pf_info">
+                        <li>여행자와 가이드를 연결해주는 플랫폼</li>
+                        <li>시술 및 고객관리 등 웹솔루션 141페이지</li>
+                        <li>고객예약 홈페이지 17페이지</li>
+                        <li>웹솔루션은 시작부터 검수까지 참여하였으며 기여도 80%</li>
+                        <li>사용자 홈페이지는 페이지작업, 검수 등 기여도 60%</li>
+                        <li>작업기간 5주</li>
+                    </ul>
+                </div>
+            </article>
+            <article class="pfArea pjec gw left" onclick="location.href='./pf/gw.php'">
+                <div class="pf_imgBx">
+                    <div class="pf_img tb"><div class="imgBx"><img src="./imgs/pf/gw/gw_pc_7.png" alt="GW main page"></div></div>
+                </div>
+                <div class="pf_tit">
+                    <h3>GW English essay center</h3>
+                    <ul class="pf_info">
+                        <li>영어 에세이 학습 센터</li>
+                        <li>영어일기, 시험, 채점이 가능한 시스템으로 38페이지</li>
+                        <li>시험지, 에세이는 형식별로 나누어 코딩하였다.</li>
+                        <li>시작부터 검수까지 참여하였으며 기여도 95%</li>
+                        <li>작업기간 3주</li>
+                    </ul>
+                </div>
+            </article>
+
+
+
+
+
             <article class="pfArea pjec center"></article>
             <article class="pfArea pjec left"></article>
             <article class="pfArea pjec right"></article>
